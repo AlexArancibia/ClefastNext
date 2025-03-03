7a21c576c40f188a334f6c0fea4ab28c62197cfe
AlexArancibia <alexanderarancibia37@gmail.com>
2025-03-03 13:40:29 -0500

    updated

--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -3,7 +3,13 @@ import ProductSkeleton from "../_components/ProductSkeleton"
 import ProductDetails from "../_components/ProductDetails"
  
 
-export default function ProductPage({ params }: { params: { id: string } }) {
+interface ProductPageProps {
+  params: {
+    id: string
+  }
+}
+
+export default function ProductPage({ params }: ProductPageProps) {
   return (
     <Suspense fallback={<ProductSkeleton />}>
       <ProductDetails id={params.id} />
